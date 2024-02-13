eb81d72b6f145da449a536b450466cf7159a0454
Muhodari <sagemuho@gmail.com>
2024-02-13 21:10:34 +0200

    added menu starter for question 1

---
+++ main.sh
@@ -0,0 +1,47 @@
+# student registration management system
+# student list file
+student_file="Students-list_1023.txt"
+
+while true; do 
+    echo "****************************************************"
+    echo "* Welcome to ALU Registration System               *"
+    echo "****************************************************"
+    echo "1. Register student                                *"
+    echo "2. View all students                               *"
+    echo "3. delete student                                  *"
+    echo "4. Update Student                                  *"
+    echo "5. Exit                                            *"
+
+    read -p " Enter choice(1-5): " choice 
+
+    case $choice in 
+	   1)
+		    echo "register student"
+
+		   read -p " Enter your Email: " email
+		   read -p " Enter Your Age: " age 
+		   read -p " Enter Student ID: " studentId 
+
+	      ;;
+           2)
+
+	          echo "view all student"
+
+	      ;;	
+
+	   3)  
+	          echo "delete student" 
+              ;;
+
+	   4)  
+	          echo "update student" 
+              ;;
+
+           5)     
+		  echo "exist"
+		  exit 0
+             ;;
+
+
+     esac
+done
