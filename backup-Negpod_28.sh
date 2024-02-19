defe2c8d037cadd313ee54c2e4288ec42ea73bd7
Muhodari <sagemuho@gmail.com>
2024-02-19 16:47:31 +0200

    fixed back up

--- backup-Negpod_28.sh
+++ backup-Negpod_28.sh
@@ -1,18 +1,26 @@
-#!/usr/bin/env bash
-#initiate remote server
-remote_server="64293e56bc62.3a2627c1.alu-cod.online"
-remote_dir="/summative/1023-2024j/"
-#source Directory
+
+#!/bin/bash
+# initiate remote server
+host="64293e56bc62.3a2627c1.alu-cod.online"
+username="64293e56bc62"
+password="328d3b338a4ced526c9a"
+remote_directory="/summative/1023-2024j/"
+
+#source Directory and backup directory
 source_dir="negpod_28-q1"
-#backup Directory
+# initiate
 backup_dir="backup-negpod_28-q1"
-#create backup directory
+# create backup folder
 mkdir "$backup_dir"
 #copy content to the backup folder
 cp -r "$source_dir" "$backup_dir"
-#send data to remove server
-scp -r "$backup_dir" "$remote_server:$remote_dir"
-#Remove the backup from our sandbox
-rm -r "$backup_dir"
 
-echo "The directory has been backed up"
+# Perform backup using rsync
+rsync -avz --progress -e "sshpass -p $password ssh -o StrictHostKeyChecking=no" "$backup_dir" "$username@$host:$remote_directory"
+
+# Check if backup is successful
+if sshpass -p "$password" ssh -o StrictHostKeyChecking=no "$username@$host" "[ -d \"$remote_directory/$backup_dir\" ]"; then
+    echo "Backup completed successfully."
+else
+    echo "Error: Backup failed."
+fi
