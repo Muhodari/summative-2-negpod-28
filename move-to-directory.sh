1ad1c34fda9f01a5919c5707a96e12c000e247d3
pauline12ish34 <paulineishimwe12@gmail.com>
2024-02-19 00:44:56 -0800

    feat: added script to move files in a directory

---
+++ move-to-directory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+directory="negpod_28-ql"
+if [ ! -d "$directory" ]; then
+	mkdir "$directory"
+	echo "Directory created: $directory"
+else
+	echo "Directory arleady exist"
+fi
+
+#move files to directory
+mv main.sh students-list_1023.txt select-emails.sh student-emails.txt "$directory/"
+
+echo "Files moved successfuly"
