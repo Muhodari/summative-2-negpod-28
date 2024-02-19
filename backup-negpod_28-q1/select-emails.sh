3aad55128e3d16955b74693ec01f1d75caf3fe90
Muhodari <sagemuho@gmail.com>
2024-02-19 21:01:56 +0200

    removed redundancy

--- backup-negpod_28-q1/select-emails.sh
+++
@@ -1,9 +0,0 @@
-#sorting emails
-studentlist="students-list_1023.txt"
-sortedemail="student-emails.txt"
-if [ -e "$studentlist" ] && [ -s "$studentlist" ]; then
-	cut -d ',' -f 1 "$studentlist" > "$sortedemail"
-else
-	echo "file does not exist or is empty"
-fi
-echo "email sorted successfully"
