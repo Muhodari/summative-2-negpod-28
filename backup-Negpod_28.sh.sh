5a8a96ea85b0b27187bdfec72b17f670c0c7456b
Muhodari <sagemuho@gmail.com>
2024-02-19 15:16:15 +0200

    added backup script

---
+++ backup-Negpod_28.sh.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#Define the remote server
+remote_server="64293e56bc62.3a2627c1.alu-cod.online"
+remote_dir="/summative/1023-2024j/"
+#Source Directory
+source_dir="negpod_28-q1"
+#Backup Directory
+backup_dir="backup-negpod_28-q1"
+#create_dir
+mkdir "$backup_dir"
+#copy content of source_dir
+cp -r "$source_dir" "$backup_dir"
+#Transfer to the remote server
+scp -r "$backup_dir" "$remote_server:$remote_dir"
+#Remove the backup from our sandbox
+rm -r "$backup_dir"
+
+echo "The directory has been backed up"
