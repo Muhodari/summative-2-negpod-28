444ba6333c92fe30995fa7595965a9513168ff62
umutonigloria <g.umutoni2@alustudent.com>
2024-02-19 23:29:04 -0800

    added proper indentation

--- backup-Negpod_28.sh
+++ backup-Negpod_28.sh
@@ -1,6 +1,7 @@
 
 #!/bin/bash
 # initiate remote server
+
 host="64293e56bc62.3a2627c1.alu-cod.online"
 username="64293e56bc62"
 password="328d3b338a4ced526c9a"
@@ -8,6 +9,7 @@ remote_directory="/summative/1023-2024j/"
 
 #source Directory and backup directory
 source_dir="negpod_28-q1"
+
 # initiate folder backup  name
 backup_dir="backup-negpod_28-q1"
 
@@ -18,8 +20,8 @@ cp -r "$source_dir" "$backup_dir"
 rsync -avz --progress -e "sshpass -p $password ssh -o StrictHostKeyChecking=no" "$backup_dir" "$username@$host:$remote_directory"
 
 #  look if the data were synced
-if sshpass -p "$password" ssh -o StrictHostKeyChecking=no "$username@$host" "[ -d \"$remote_directory/$backup_dir\" ]"; then
-    echo "back up process successfully completed."
-else
-    echo " Error occured while backup proceess under going"
+   if sshpass -p "$password" ssh -o StrictHostKeyChecking=no "$username@$host" "[ -d \"$remote_directory/$backup_dir\" ]"; then
+         echo "back up process successfully completed."
+    else
+         echo " Error occured while backup proceess under going"
 fi
