6370a4068df4305595e233df9c7452824e34fe7b
Alliance-D <dushimezi@alustudent.com>
2024-02-25 10:32:09 -0800

    updated backup-Negpod_28.sh

--- backup-Negpod_28.sh
+++ backup-Negpod_28.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# initiate remote server.
+
+#initiate remote server.
 
 host="64293e56bc62.3a2627c1.alu-cod.online"
 username="64293e56bc62"
@@ -9,16 +10,16 @@ remote_directory="/summative/1023-2024j/"
 #source Directory and backup directory
 source_dir="negpod_28-q1"
 
-# initiate folder backup  name
+#initiating folder backup  name
 backup_dir="backup-negpod_28-q1"
 
 #copy content to the backup folder
 cp -r "$source_dir" "$backup_dir"
 
-# send data to the remote server
+#send data to the remote server
 rsync -avz --progress -e "sshpass -p $password ssh -o StrictHostKeyChecking=no" "$backup_dir" "$username@$host:$remote_directory"
 
-#  look if the data were synced
+#look if the data were synced
    if sshpass -p "$password" ssh -o StrictHostKeyChecking=no "$username@$host" "[ -d \"$remote_directory/$backup_dir\" ]"; then
          echo "back up process successfully completed."
     else
